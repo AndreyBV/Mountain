589343570fc366ac1a55e883298400a590ccd1c2
Andrey Barsukov <andrey.bv.gm@gmail.com>
2021-11-01 16:03:32 +0500

    Fix mobile navigation (fixed nav. button, adaptive 520px); Animation scroll on click hero block.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,38 @@
 const navBtn = document.querySelector('.nav-button');
 const mobileNav = document.querySelector('.mobile-nav');
+const body = document.body;
 
-navBtn.addEventListener('click', function () {
+// Navigation button click
+navBtn.addEventListener('click', function (event) {
+	event.stopPropagation();
+	toggleMobileNav();
+});
+
+// Window click except navigation panel
+window.addEventListener('click', function () {
+	// If user clicked behind the navigation panel, then hidden nav. panel
+	if (mobileNav.classList.contains('mobile-nav-active')) {
+		toggleMobileNav();
+	}
+});
+
+// Except navigation panel on click window
+mobileNav.addEventListener('click', function (event) {
+	event.stopPropagation();
+});
+
+function toggleMobileNav() {
+	// body.classList.toggle('no-scroll');
 	mobileNav.classList.toggle('mobile-nav-active');
 	navBtn.classList.toggle('nav-button-close');
+}
+
+// !Experiment - smooth scrolling animation
+$(document).ready(function () {
+	$('.hero-wrapper').on('click', 'a', function (event) {
+		event.preventDefault();
+		var id = $(this).attr('href'),
+			top = $(id).offset().top - 150;
+		$('body,html').animate({ scrollTop: top }, 1200);
+	});
 });
